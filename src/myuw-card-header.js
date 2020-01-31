6f4bffc9cbf8020ea56bf4d5542b3f632263da98
Andy Summers <andrew.summers@wisc.edu>
2020-01-31 07:14:22 -0700

    Improve accessibility of MyUW Card (#11)

--- src/myuw-card-header.js
+++ src/myuw-card-header.js
@@ -27,9 +27,9 @@ export class MyUWCardHeader extends HTMLElement {
         </style>
 
         <slot></slot>
-        <svg class="more-icon" width="20" height="20">
+        <svg class="more-icon" width="20" height="20" role="button" tabindex=0 aria-label="more menu button">
           <g transform="scale(0.8)">
-              <path d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" />
+            <path  aria-label="more menu icon" d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" />
           </g>
         </svg>
       `;
